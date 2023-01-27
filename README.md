f4eedfe33be11653597b2ccee7c7b97ff49c0c5c
Boula Akladyous <akladyous@gmail.com>
2023-01-27 18:00:26 -0500

    aa

--- README.md
+++
@@ -1,3 +0,0 @@
-# nextjs
-# nextjs
-# nextjs
---
+++ my-app/pages/home.js
@@ -0,0 +1,21 @@
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return {
+    props: { users: data },
+  };
+}
+
+export default function home({ users }) {
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p> 2<p>{user.email}</p> 2
+          </div>
+        );
+      })}
+    </div>
+  );
+}
