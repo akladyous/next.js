e2f2259c5e31aa05fa8bdf0c1393c92693eaf044
Boula Akladyous <akladyous@gmail.com>
2023-04-20 22:27:26 -0400

    Untracked files on nextjs

--- next01/app/about/about.tsx
+++ next01/app/about/about.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 
-export default function anout() {
-  return <div>anout</div>;
+export default function About() {
+  return <div className=' styles.main'>anout</div>;
 }
