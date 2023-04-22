0fe8d6e204bcabb43e0753ec373037b129a070d2
Boula Akladyous <akladyous@gmail.com>
2023-04-22 12:43:36 -0400

    Untracked files on nextjs

--- next01/lib/getUser.tsx
+++ next01/lib/getUser.tsx
@@ -1,5 +1,6 @@
 export default async function getUser(userId: string, options?: {}) {
   var response: Response;
+  if (userId === '') throw new Error('Missing User ID');
   debugger;
   try {
     const url: string = `https://jsonplaceholder.typicode.com/user/${userId}`;
