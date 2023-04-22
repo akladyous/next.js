28e5487175f01807dc8863118894a42e5e0d4a38
Boula Akladyous <akladyous@gmail.com>
2023-04-22 15:21:44 -0400

    Untracked files on nextjs

--- next01/app/users/[userId]/page.tsx
+++ next01/app/users/[userId]/page.tsx
@@ -1,6 +1,6 @@
-import { Suspense } from 'react';
 import getUser from '@/lib/getUser';
 import getUserPosts from '@/lib/getUserPosts';
+import { Suspense } from 'react';
 import UserPosts from './components/UserPosts';
 type Params = {
   params: {
@@ -9,10 +9,11 @@ type Params = {
 };
 
 export default async function UserPage({ params: { userId } }: Params) {
-  const userData: Promise<User> = getUser(userId);
+  const userDataPromise: Promise<User> = getUser(userId);
   const userPostsData: Promise<Post[]> = getUserPosts(userId);
 
-  // const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  // const [user, userPosts] = await Promise.all([userDataPromise, userPostsData]);
+  const user = await userDataPromise;
 
   return (
     <div>
