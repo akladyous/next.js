6d18322beb145099d69d063938e07ab151057537
Boula Akladyous <akladyous@gmail.com>
2023-04-23 13:09:48 -0400

    Untracked files on nextjs

--- next01/app/users/[userId]/components/UserPosts.tsx
+++ next01/app/users/[userId]/components/UserPosts.tsx
@@ -6,7 +6,7 @@ export default async function UserPosts({ promise }: Props) {
   const posts = await promise;
   const content = posts.map((post) => {
     return (
-      <article>
+      <article key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
         <br />
