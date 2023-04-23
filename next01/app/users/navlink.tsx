a55031dc8cce129c9d4715447c1899a91faf059b
Boula Akladyous <akladyous@gmail.com>
2023-04-23 12:08:44 -0400

    Untracked files on nextjs

--- next01/app/users/navlink.tsx
+++ next01/app/users/navlink.tsx
@@ -1,8 +1,12 @@
 'use client';
-
+import { useEffect } from 'react';
 import Link, { LinkProps } from 'next/link';
 
-import { useSelectedLayoutSegment } from 'next/navigation';
+import {
+  useSelectedLayoutSegment,
+  usePathname,
+  useSearchParams,
+} from 'next/navigation';
 
 // React.AllHTMLAttributes<HTMLAnchorElement>
 interface NavLinkProps extends LinkProps {
@@ -16,16 +20,28 @@ function classNames(...classes: string[]) {
 
 export default function NavLink({ href, children, className }: NavLinkProps) {
   const segment = useSelectedLayoutSegment();
-  let isActive = href === `/${segment}`;
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  let active = href === `/${segment ? segment : ''}`;
   console.log('NavLink -> href:     ', href);
-  console.log('NavLink -> segment:  ', segment);
   console.log('NavLink -> children: ', children);
+  console.log('NavLink -> is active: ', active);
   console.log('-'.repeat(50));
+
+  useEffect(() => {
+    console.log('searchParams : ', searchParams);
+    console.log('pathname     : ', pathname);
+  }, []);
 
   return (
     <>
       <Link
-        {...(className && { className })}
+        // {...(className && { className })}
+        className={classNames(
+          active ? 'bg-gray-100 text-gray-900' : '',
+          'text-gray-900  hover:bg-blue-400 p-2 rouned-lg',
+        )}
         href={href}
       >
         {children}
