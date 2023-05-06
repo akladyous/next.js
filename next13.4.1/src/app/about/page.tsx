da733307f73d2eddf4e0e79f9445772174c36e64
Boula Akladyous <akladyous@gmail.com>
2023-05-06 14:39:58 -0400

    Untracked files on nextjs

--- next13.4.1/src/app/about/page.tsx
+++ next13.4.1/src/app/about/page.tsx
@@ -1,13 +1,21 @@
+import { Roboto } from 'next/font/google';
 import type { Metadata } from 'next';
+import { Robots } from 'next/dist/lib/metadata/types/metadata-types';
 
 export const metadata: Metadata = {
   title: 'About Page',
   description: 'Welcome to About Page',
 };
+const roboto = Roboto({
+  subsets: ['latin'],
+  display: 'block',
+  weight: '500',
+});
+
 export default function AboutPage() {
   return (
     <main className=''>
-      <h3>About Page</h3>
+      <h3 className={`${roboto.className} mb-3 text-2xl `}>About Page</h3>
     </main>
   );
 }
