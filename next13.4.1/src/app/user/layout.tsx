f3fa986e97a0c94d32b1f3efbd83ec84cd99bfe6
Boula Akladyous <akladyous@gmail.com>
2023-05-09 14:21:01 -0400

    Untracked files on next13.4.1

--- next13.4.1/src/app/user/layout.tsx
+++ next13.4.1/src/app/user/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from 'next';
 
-async function getProduct(id: string) {
-  const res = await fetch(`https://.../api/products/${id}`);
-  return res.json();
-}
 type UserProps = {
   children: React.ReactNode;
   params: { user_id: string };
 };
+
+async function getProduct(id: string) {
+  const res = await fetch(`https://.../api/products/${id}`);
+  return res.json();
+}
 
 export async function generateMetadata(props: {}): Promise<Metadata> {
   // const product = await getProduct(params.id);
