48591f7a396582be3c43199ddf6933b0d5f0572d
Boula Akladyous <akladyous@gmail.com>
2023-05-10 19:22:58 -0400

    Untracked files on next13.4.1

--- next13.4.1/src/app/page.tsx
+++ next13.4.1/src/app/page.tsx
@@ -1,10 +1,13 @@
 import ActiveLink from './activeLinkes';
+import NavLink from './user/components/NavLink';
 
-export default function Home() {
+export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <section className='border-green-500 border-2 m-2 p-2 min-h-screen'>
       <h3>Home Page</h3>
       <ActiveLink />
+
+      <NavLink href='prova'>Prova Page...</NavLink>
     </section>
   );
 }
--- next13.4.1/src/app/user/components/NavLink.tsx
+++ next13.4.1/src/app/user/components/NavLink.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useSelectedLayoutSegment } from 'next/navigation';
 
 type Props = {
   href: string;
@@ -6,5 +7,19 @@ type Props = {
 };
 
 export default function NavLink({ href, children }: Props) {
-  return <></>;
+  const segment = useSelectedLayoutSegment();
+  const isActive = href === segment;
+  console.log('segment : ', segment);
+  console.log('isActive : ', isActive);
+
+  return (
+    <>
+      <Link
+        href={`${href}`}
+        className={isActive ? 'font-bold' : 'underline'}
+      >
+        {children}
+      </Link>
+    </>
+  );
 }
