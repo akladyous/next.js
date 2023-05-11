aa07dba83f3813fc747046b5195ff2554d256a0c
Boula Akladyous <akladyous@gmail.com>
2023-05-10 20:25:09 -0400

    Untracked files on next13.4.1

--- next13.4.1/src/app/user/layout.tsx
+++ next13.4.1/src/app/user/layout.tsx
@@ -1,3 +1,9 @@
+'use client';
+import {
+  useSelectedLayoutSegment,
+  useSelectedLayoutSegments,
+} from 'next/navigation';
+
 type UserProps = {
   children: React.ReactNode;
   params: { user_id: string };
@@ -5,6 +11,16 @@ type UserProps = {
 
 export default function UserHomeLayout(props: UserProps) {
   console.log('\x1b[36m%s%s\x1b[0m', 'UserLayout props : '.padEnd(50), props);
+  console.log(
+    '\x1b[36m%s%s\x1b[0m',
+    'UserLayout useSelectedLayoutSegment : '.padEnd(50),
+    useSelectedLayoutSegment(),
+  );
+  console.log(
+    '\x1b[36m%s%s\x1b[0m',
+    'UserLayout useSelectedLayoutSegments : '.padEnd(50),
+    useSelectedLayoutSegments(),
+  );
 
   return (
     <main
