f7c8c8706deb0e5102945d6358fc924ef1bbc4cd
Boula Akladyous <akladyous@gmail.com>
2023-05-12 14:25:12 -0400

    Untracked files on next13.4

--- next13.4/src/app/page.tsx
+++ next13.4/src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <section className='border-green-500 border-2 m-2 p-2 min-h-screen'>
       <h3>Home Page</h3>
+      {children}
     </section>
   );
 }
