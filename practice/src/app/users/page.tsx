38467730fb9eed9bbb9768ced4c98d37a6906276
Boula Akladyous <akladyous@gmail.com>
2023-06-04 14:30:42 -0400

    Untracked files on practice

--- practice/src/app/users/page.tsx
+++ practice/src/app/users/page.tsx
@@ -1,4 +1,8 @@
+import { getAllUsers } from '@/lib/getAllUsers';
+
 export default function FetchPage(props: {}) {
+  const usersData: Promise<{}> = getAllUsers();
+
   return (
     <main>
       <h3>Fetch Data</h3>
--- practice/src/lib/getAllUsers.tsx
+++ practice/src/lib/getAllUsers.tsx
@@ -1,4 +1,4 @@
-async function getAllUsers(): Promise<any> {
+export async function getAllUsers(): Promise<any> {
   const baseURL: string = 'https://jsonplaceholder.typicode.com/';
   const response = await fetch(baseURL, { next: { revalidate: 60 } });
   if (!response.ok) throw new Error('Error Fetching Data');
