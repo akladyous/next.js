e61e503ca58fba96772d6fc6060a90bec7a71ad4
Boula Akladyous <akladyous@gmail.com>
2023-06-04 13:23:44 -0400

    Untracked files on nextjs

--- practice/src/app/api/route.ts
+++ practice/src/app/api/route.ts
@@ -1,40 +1,6 @@
-import { NextResponse } from 'next/server';
+import { type NextRequest } from 'next/server';
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  console.log('-'.repeat(60));
-  console.log('searchParams : ', searchParams);
-  console.log('-'.repeat(60));
-  // const id = searchParams.get('id');
-  // const res = await fetch(`https://data.mongodb-api.com/product/${id}`, {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'API-Key': process.env.DATA_API_KEY,
-  //   },
-  // });
-  // const product = await res.json();
-
-  return NextResponse.json({
-    id: 1,
-    name: 'Leanne Graham',
-    userName: 'Bret',
-    email: 'Sincere@april.biz',
-    phone: '1-770-736-8031 x56442',
-    website: 'hildegard.org',
-    address: {
-      street: 'Kulas Light',
-      suite: 'Apt. 556',
-      city: 'Gwenborough',
-      zipcode: '92998-3874',
-      geo: {
-        lat: '-37.3159',
-        lng: '81.1496',
-      },
-    },
-    company: {
-      name: 'Romaguera-Crona',
-      catchPhrase: 'Multi-layered client-server neural-net',
-      bs: 'harness real-time e-markets',
-    },
-  });
+export async function GET(request: NextRequest) {
+  const token = request.cookies.get('token');
+  console.log('token : ', token);
 }
