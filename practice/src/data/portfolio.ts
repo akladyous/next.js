18efc56e8a8036d8a1a9200316c218d121b2fcda
Boula Akladyous <akladyous@gmail.com>
2023-06-05 16:34:48 -0400

    Untracked files on practice

---
+++ practice/src/data/portfolio.ts
@@ -0,0 +1,61 @@
+export const portfolio = {
+	achievements: [
+		{ title: "4", subTitle1: "years of", subTitle2: "experience" },
+		{ title: "83", subTitle1: "completed", subTitle2: "projects" },
+		{ title: "27", subTitle1: "Happy", subTitle2: "customers" },
+		{ title: "16", subTitle1: "awards", subTitle2: "won" },
+	],
+
+	skills: [
+		{ skillPercent: "86", skillName: "JAVA" },
+		{ skillPercent: "86", skillName: "SPRING" },
+		{ skillPercent: "95", skillName: "JAVASCRIPT" },
+		{ skillPercent: "90", skillName: "PYTHON" },
+		{ skillPercent: "95", skillName: "HTML" },
+		{ skillPercent: "85", skillName: "CSS" },
+		{ skillPercent: "90", skillName: "REACT" },
+		{ skillPercent: "87", skillName: "Next.js" },
+	],
+
+	experiences: [
+		{
+			year: "Dec 2022 - Present",
+			position: "Full Stack Developer",
+			compnayName: "Self-Employed",
+			details: "Java · Spring ·JavaScript · TypeScript · Redux · NextJS ",
+		},
+		{
+			year: "Jan 2020 - Nov 2022",
+			position: "Full-Stack Java Developer",
+			compnayName: "Trust Investing",
+			details: `Java · Spring · Junit · JavaScript · React · BootStrap · docker · Git`,
+		},
+		{
+			year: "Feb 2017 - Jul 2019",
+			position: "Backend Developer",
+			compnayName: "Amadeus",
+			details: `JavaScript · ExpressJS · Python · pandas · SQL`,
+		},
+	],
+
+	educations: [
+		{
+			year: "2021",
+			degree: "Software Engineering",
+			institute: "Flatiron School",
+			details: "Immersive Software Engineering Bootcamp Program",
+		},
+		{
+			year: "2020",
+			degree: "Data Science",
+			institute: "Flatiron School",
+			details: "Immersive Data Science Bootcamp Program",
+		},
+		{
+			year: "1994",
+			degree: "Computer Science",
+			institute: "advanced industrial technical institute",
+			details: `Advanced industrial technical institute`,
+		},
+	],
+};
