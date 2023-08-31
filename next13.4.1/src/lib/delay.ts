e4ad12f663dfc1c32fea12f20948a07b9f860580
boula akladyous <boulaakladyous@boulas-MacBook-Pro.local>
2023-08-31 13:33:01 -0400

    Untracked files on next.js

--- next13.4.1/src/lib/delay.ts
+++
@@ -1,8 +0,0 @@
-export default async function delay(timeout = 1000) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log('delay function');
-      resolve({ user_id: 1 });
-    }, timeout);
-  });
-}
--- next13.4.1/src/lib/getUsers.tsx
+++
@@ -1,8 +0,0 @@
-export const getUsers = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users', {
-    next: { revalidate: 60 },
-  });
-
-  if (!response.ok) return undefined;
-  return response.json();
-};
