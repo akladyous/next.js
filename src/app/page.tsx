58ea2081303ae88c550c7747ce1013daf392a8f2
Boula Akladyous <akladyous@gmail.com>
2023-09-03 13:53:42 -0400

    Untracked files on next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main className='flex min-h-screen'></main>;
+  return (
+    <main className='flex min-h-screen'>
+      <h4>Next.JS</h4>
+    </main>
+  );
 }
